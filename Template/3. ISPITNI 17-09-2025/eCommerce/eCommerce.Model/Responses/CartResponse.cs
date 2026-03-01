a5fcdd2e7f32bf4ea8fba18cf2929581b1d72e78
benjamin-cero <benjamin.cero25@gmail.com>
2026-03-01 02:55:41 +0100

    Cart Response

---
+++ Template/3. ISPITNI 17-09-2025/eCommerce/eCommerce.Model/Responses/CartResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Model.Responses
+{
+    public class CartResponse
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public List<CartItemResponse> Items { get; set; } = new List<CartItemResponse>();
+
+    }
+}
